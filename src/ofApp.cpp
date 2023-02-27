084de8836df294032b88ab26b83301761b89183d
ChiliOlavi <115644711+ChiliOlavi@users.noreply.github.com>
2023-02-27 14:46:51 +0200

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,133 +2,93 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofSetColor(255);
-	myButton.set(100,100,50,50);
-	//Image stuff:
-	/*myImage.load("100.jpg");
-	myImage.resize(800, 272);
-	myPixels = myImage.getPixels();*/
-	//Other stuff:
+	//UI Setup
+	myFont.load("AGENCYB.ttf", 32);
 	ofBackground(0);
+	myButton.set(100, 70, 300, 70);
+	
+
+	//Load the embeddings file:
 	string file_name = "vec_text8.bin";
 	embed.load_binary(file_name);
-	string f = "";
-	string s = "";
-	/*string s = "dog+ tree";
-	vector<int> used_indices;
-	ofxWord2VecVector Vec = embed.words_to_vec(s, &used_indices);*/
+	
+	//Strings for text:
+	string f = " ";
+	string s = " ";
+	
+	//Set animation zooming speed:
+
+	zoom = -2;
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-	/*string s = "dog + tree";
-	vector<int> used_indices;
-	ofxWord2VecVector Vec = embed.words_to_vec(s, &used_indices);*/
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-	//ofDrawRectangle(myButton);
-	//activate = false;
-	//ofPushMatrix();
-	//ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2, -100);
-	string f = "man + dog + snow + plow + hello + my + name + is + eagle";
-	ofDrawBitmapString(f, 300, 400);
-	//if (activate) {
-	s=f;
-
+void ofApp::update() {
+	//Calculate the vector
+	if (calculate) {
+		s = f;
 		vector<int> used_indices;
-		ofxWord2VecVector Vec = embed.words_to_vec(s, &used_indices);
-		//string vectorString = ofToString(Vec);
-		//string vectorString=ofToString(Vec.print_console("a", "a", 2));
-		/*for (int i = 0; i < 500; i++)
-		{
-			ofToString(Vec[i]) = myStrings[i];
+		Vec = embed.words_to_vec(s, &used_indices);
+		if (!Vec.empty()) {
+
+			auto match = embed.match_cos(Vec, 1, used_indices); //Get new vector that is closest to the calculated one
+			n = ofToString(match[0].word);
+
 		}
-		 string vectorString=ofToString(Vec.print_console("a", "a", 2));*/
-		 //ofDrawBitmapString(vectorString,100,100);
-		 // ofDrawCircle(50, 50, 50);
-		  //path.arc();
-		vector <ofPoint> points;
-		//poly.translate(ofPoint(1000, 1000, 0));
-		for (int i = 0; i < 200; i++) {
-			/*for (int j = 0; j < 200; j++) {
-				for (int k = 0; k < 200; k++)
-				{
-					ofSetColor(255 - i, 150 + ((j + 2)), 100 + k, 90);
-					path.lineTo(Vec.v[i], Vec.v[j], Vec.v[k]);
-				}
-
-			}*/
-			//ofSetColor(255 - i, 150 + ((i + 2)), 100 + i, 50);'
-
-			// points.push_back((ofPoint((Vec.v[i] * i), (Vec.v[i + 1] * i), (Vec.v[i + 2] * i))));
-				//path.lineTo(Vec.v[i], Vec.v[i+1], Vec.v[i+2]);
-			/* for (int x = 0; x < myImage.getWidth(); x++) {
-				 for (int y = 0; y < myImage.getHeight(); y++) {
-					 ofSetColor(myPixels.getColor(x, y));
-					 ofDrawTriangle((Vec.v[i] * i), (Vec.v[i] * i) + 30, (Vec.v[i + 1] * i), (Vec.v[i + 1] * i) + 30, (Vec.v[i + 2] * i), (Vec.v[i + 2] * i) + 30);
-				 }
-			 }*/
-			ofPushMatrix();
-			ofTranslate(ofGetWidth() * 0.5, ofGetHeight() * 0.5, (-i) * 2);
-			//Some CRAZY STUFF:
-			 //ofSetColor(int(ofRandom(0, 255) + Vec.v[i]), int(ofRandom(0, 255) + Vec.v[i + 1]), int(ofRandom(0, 255) + Vec.v[i]));
-			ofSetColor(i, i, i, 20);
-			poly.addVertex(ofPoint((Vec.v[i] * i), (Vec.v[i + 1] * i), (Vec.v[i + 2] * i)));
-			poly.close();
-			poly.draw();
-			ofPopMatrix();
-			// points.push_back(ofPoint((Vec.v[i]*i), (Vec.v[i+1]*i), (Vec.v[i + 2]*i)));
-	//path.lineTo(Vec.v[i]*i, Vec.v[i + 1]*i, Vec.v[i + 2]*i);
-				 //ofDrawLine(Vec.v[i] * 10, Vec.v[j] * 10, Vec.v[i] * 10, Vec.v[j] * 10);
-
-
-			cout << "Vector" << Vec.v[i] << endl;
-		}
-
-		// path.close();
-		// path.draw();
-		//poly.close();
-		//poly.getVertices();
-		/*for (int x = 0; x < myImage.getWidth(); x++) {
-			for (int y = 0; y < myImage.getHeight(); y++) {
-
-				ofSetColor(myPixels.getColor(x, y));
-				poly.draw();
-			}
-		}*/
-		//poly.draw();
-
-		// ofSetFrameRate(5);
-
-
-		// poly.draw();
-		 //line.close();
-		cout << "End" << endl;
-
-	//}
+		visualize = true;
+	}
 }
+
+	//--------------------------------------------------------------
+	void ofApp::draw() {
+
+		if (visualize) {
+
+			//Go through the values in vector:
+			for (int i = 0; i < 200; i++) {
+				ofPushMatrix();
+				
+				//Animate:
+				ofTranslate(ofGetWidth() * 0.5, ofGetHeight() * 0.5, i* (zoom));
+				
+				//Colour the shape:
+				ofSetColor(i, i, i, 20);
+				//Alternative colour schemes:
+				//ofSetColor(Vec.v[i]+i, Vec.v[i + 1]+i, Vec.v[i + 1]+i,10); //Vector based
+				//ofSetColor(int(ofRandom(0, 255) + Vec.v[i]), int(ofRandom(0, 255) + Vec.v[i + 1]), int(ofRandom(0, 255) + Vec.v[i])); //Trippy
+
+				//Draw the shape:
+				poly.addVertex(ofPoint((Vec.v[i] * i), (Vec.v[i + 1] * i), (Vec.v[i + 2] * i)));
+				poly.close();
+				poly.draw();
+				ofPopMatrix();
+
+				//Display vector in console:
+				cout << "Vector" << Vec.v[i] << endl;
+			}
+		}
+		//Draw UI:
+		ofNoFill();
+		ofSetColor(255, 0, 0);
+		ofDrawRectangle(myButton);
+		myFont.drawString("Calculate vector", 120, 120);
+		myFont.drawString(f, 300, 400);
+		if (visualize) {
+			myFont.drawString("= " + n, 300, 500);
+		}
+
+	}
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-	if (key == '\n') { //press enter to calculate vector
-
-		activate == true;
-
-	}
-
-	if (key == '8' && s.size() > 0) { // backspace
+	
+	//Keyboard inputs:
+	if (key == 8 && f.size() > 0) { // backspace
 		f = f.substr(0, f.size() - 1); // delete one character
 	}
-	else { //send characters to screen
+	else { //send characters to that get vectorized
 		f.append(1, (char)key);
 	}
-	
-	
 }
-
-
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
@@ -147,7 +107,10 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-	if(myButton.inside(x,y))activate==true;
+	//Button
+	if (myButton.inside(x, y)) {
+		calculate = true;
+	}
 }
 
 //--------------------------------------------------------------
